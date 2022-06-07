7e734efb66613f8b9d5f13370cb4a7865bc20f90
MalcolmorianVII <bdkutambe@gmail.com>
2022-06-07 16:59:52 +0200

    Added the db model

--- mobtype.py
+++ mobtype.py
@@ -7,6 +7,15 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "plasmid.db"
 
 db = SQLAlchemy(app)
 
+class Inctype(db.Model):
+    sample_id = db.Column(db.String(30),primary_key=True)
+    rep_type = db.Column(db.String(60),nullable=False)
+    rep_type_accession = db.Column(db.String(60),nullable=False)
+    predicted_mobility = db.Column(db.String(60),nullable=False)
+    organism = db.Column(db.String(60),nullable=False)
+    taxid = db.Column(db.Integer,nullable=False)
+    
+
 @app.route("/")
 def home():
     return render_template("home.html")
