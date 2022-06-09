9aeb51843e9c36c58c58d5e6fb25f46138d1a93d
MalcolmorianVII <bdkutambe@gmail.com>
2022-06-09 21:19:00 +0200

    Added the sqlite to db path

--- mobtype.py
+++ mobtype.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "plasmid.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///plasmid.db"
 
 db = SQLAlchemy(app)
 
